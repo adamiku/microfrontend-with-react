d074e3977a501e3987c3bf4f325e943c3c33498d
Adam <adam_mikulas@mckinsey.com>
2023-06-26 17:39:53 +0200

    feat: add marketing to container

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,7 +1,8 @@
 const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
-
+const packageJson = require("../package.json");
 const devConfig = {
   mode: "development",
   devServer: {
@@ -10,7 +11,17 @@ const devConfig = {
       index: "index.html",
     },
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({ template: "./public/index.html" }),
+  ],
 };
 
 module.exports = merge(commonConfig, devConfig);
