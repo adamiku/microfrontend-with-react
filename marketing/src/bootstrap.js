ef5185d261386c78dc8edc3a8ff2c0eec120142c
Adam <adam_mikulas@mckinsey.com>
2023-06-28 14:49:57 +0200

    feat: add routing callbacks to container and marketing apps

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,33 @@
+import { createBrowserHistory, createMemoryHistory } from "history";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 //Mount function to start up the app
-const mount = (element) => {
-  ReactDOM.render(<App />, element);
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory ?? createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, element);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in development and in isolation, call mount immediately
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
